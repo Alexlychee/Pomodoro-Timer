4ba09513c3cc3610bc70f8797a0a3d5febbf5322
Alex Ly <70249534+Alexlychee@users.noreply.github.com>
2024-10-11 19:01:20 -0700

    added countdown functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -13,8 +14,15 @@ THUMBS_UP = "✔"
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+def start_timer():
+    countdown(WORK_MIN * 60)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def countdown(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, countdown, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -27,11 +35,11 @@ timer_label.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME,  35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME,  35, "bold"))
 canvas.grid(column=1, row=1)
 
 # Start Button
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", command=start_timer ,highlightthickness=0)
 start_button.grid(column=0, row=2)
 
 # Reset Button

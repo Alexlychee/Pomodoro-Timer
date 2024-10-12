fc4293fac0cd34004e90df83d5e6778e31a72364
Alex Ly <70249534+Alexlychee@users.noreply.github.com>
2024-10-11 19:06:10 -0700

    fixed issue with countdown not displaying correct leading zeros

--- main.py
+++ main.py
@@ -15,11 +15,13 @@ THUMBS_UP = "✔"
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    countdown(WORK_MIN * 60)
+    countdown(1 * 60)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def countdown(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, countdown, count - 1)
